513e5191191ac06de0f00ac9f6048c8d31458ab8
Neramit <DavidTh30@gmail.com>
2025-12-31 14:34:56 +0700

    Add files via upload

---
+++ ASCII Art01.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Example ASCII Art
+echo.
+chcp 65001 > nul
+
+echo ┌─────────────┐
+echo │ ╔═════════╗ │
+echo │ ║ Success!║ │
+echo │ ║ █▓▒░█▓▒░║ │
+echo │ ╚═════════╝ │
+echo └─────────────┘
+echo.
+echo    ___
+echo   [□^|□]
+echo   [=_=]
+echo ┌───┘
+echo │ ╔══════════════╗
+echo │ ║              ║
+echo │ ║              ║
+echo │ ║              ║
+echo █ ║              ║
+echo │ ╚══════════════╝
+echo └/□□□□□□□□□□□□□□□□\
+echo /□□□□□■■■■■■■■□□□□□\
+echo ════════════════════
+chcp 1252 > nul 
+
+pause
