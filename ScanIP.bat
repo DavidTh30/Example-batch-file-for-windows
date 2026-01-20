c35c8e43d416a88ae8e606e1971538d95449c6cd
Neramit <DavidTh30@gmail.com>
2026-01-20 17:06:45 +0700

    Update ScanIP.bat

--- ScanIP.bat
+++ ScanIP.bat
@@ -10,10 +10,11 @@ echo Scanning network %IP_PREFIX%%START_IP% to %IP_PREFIX%%END_IP%...
     FOR /L %%i IN (%START_IP%,1,%END_IP%) DO (
         ping -n 1 -w 100 !IP_PREFIX!%%i | findstr "Reply from" >NUL && (
             echo !IP_PREFIX!%%i
-            echo > "%OUTPUT_FILE%" !IP_PREFIX!%%i
+            echo >> "%OUTPUT_FILE%" !IP_PREFIX!%%i
         )
     )
 
 echo Scan complete. Results saved to %OUTPUT_FILE%.
 ENDLOCAL
+
 pause
